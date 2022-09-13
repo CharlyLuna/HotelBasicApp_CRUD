cd54bb1e17986b7ac0733c6ea9cdf91a28a0f990
CharlyLuna <cluna10@ucol.mx>
2022-09-12 21:39:33 -0500

    Implement connection to MongoDB

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const dbConnect = require("../config/mongo");
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -7,3 +8,5 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
+
+dbConnect();
