ce9c51a286cad19783e41820f58b923654781ae4
CharlyLuna <cluna10@ucol.mx>
2023-05-14 22:54:02 -0600

    Add cors origin for localhost env

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const dbConnect = require("./config/mongo");
 const app = express();
+const cors = require('cors')
+
+app.use(cors({
+  origin: 'http://localhost:5173'
+}))
 
 app.use(express.json());
 
