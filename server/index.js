88c7c89d8c524e72bc5e537896e701e148b352d5
CharlyLuna <cluna10@ucol.mx>
2023-05-14 19:52:10 -0600

    Re-structure of project files

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 const express = require("express");
-const dbConnect = require("../config/mongo");
+const dbConnect = require("./config/mongo");
 const app = express();
 
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
-app.use("/api", require("../routes"));
+app.use("/api", require("./routes"));
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
