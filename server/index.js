edce7cf009dee2af15391c34c008865f5a42c22c
CharlyLuna <cluna10@ucol.mx>
2023-05-25 22:17:11 -0600

    Change cors for testing

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = express();
 const cors = require('cors')
 
 app.use(cors({
-  origin: ['http://localhost:5173','https://hotel-app-crud.netlify.app/']
+  origin: '*'
 }))
 
 app.use(express.json());
